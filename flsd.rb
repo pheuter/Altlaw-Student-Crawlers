0b0e2dfcf9a1aa157eea9dc90b547104b4d120b5
Alec Benzer <alec@umbra.(none)>
2009-08-14 12:29:40 -0400

    revised flsd to give different doucments different Document objects, regardless of whether or not they belong to the same case

--- flsd.rb
+++ flsd.rb
@@ -19,33 +19,21 @@ class Flsd
     doc = Hpricot(download.response_body_as('US-ASCII'))
     cases =  doc.search("table/tr/td[@align='left']")
     cases.each do |c|
-      document = nil
-      new_doc = true
+      document = Document.new
       info = c.at("font b i").inner_html
       docket = info.split.first
-      receiver.each do |a_doc|
-        a_doc.dockets.each do |a_docket|
-          if a_docket == docket
-            document = a_doc
-            new_doc = false
-          end
-        end
-      end
       
-      if new_doc
-        document = Document.new
-        document.dockets << docket
-        doc_name = ""
-        info.reverse.split('').each do |char|
-          break if char == '-'
-          doc_name << char
-        end
-        document.name = doc_name.reverse.strip
-#        c.inner_html =~ /\((\d{2})\/(\d{2})\/(\d{4})\)/
-        md = match(c.inner_html,/\((\d{2})\/(\d{2})\/(\d{4})\)/)
-        document.date = Date.new(md[3].to_i, md[1].to_i, md[2].to_i)
-        document.court = 'http://id.altlaw.org/courts/us/fed/dist/flsd'
+      document.dockets << docket
+      doc_name = ""
+      info.reverse.split('').each do |char|
+        break if char == '-'
+        doc_name << char
       end
+      document.name = doc_name.reverse.strip
+#        c.inner_html =~ /\((\d{2})\/(\d{2})\/(\d{4})\)/
+      md = match(c.inner_html,/\((\d{2})\/(\d{2})\/(\d{4})\)/)
+      document.date = Date.new(md[3].to_i, md[1].to_i, md[2].to_i)
+      document.court = 'http://id.altlaw.org/courts/us/fed/dist/flsd'
 
       anchor_tag = c.at("a")
       raise "could not find anchor tag" unless anchor_tag
@@ -56,7 +44,7 @@ class Flsd
         raise Exception.new("could not find pdf name")
       end
 
-      receiver << document if new_doc
+      receiver << document
     end
   end
 end
