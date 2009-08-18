a41c9ecb2d9193bf6ce50e4d978e4804bf8a13d2
Alexander Lam <lambchop468@gmail.com>
2009-08-18 00:24:35 -0400

    ksd.rb: Alec Benzer pointed out that https://ecf.ksd.uscourts.gov/cgi-bin/Opinions.pl would return cases from all years, allowing simplification of much of the fetch code. Also, Added support for 'subject' field for document.

--- ksd.rb
+++ ksd.rb
@@ -4,38 +4,20 @@
 # July 26, 2009: First Revision
 # August 14, 2009: Fixed bug: BASEURL contained Opinions.pl? which is incorrect.
 # August 14, 2009: Changed regex that splits cell 2 into the Docket and Case Name to use the match() function.
+# August 17, 2009: Alec Benzer pointed out that https://ecf.ksd.uscourts.gov/cgi-bin/Opinions.pl would return cases from all years, allowing simplification of much of the fetch code.
+# August 17, 2009: Added support for 'subject' field for document.
 class Ksd
-	START_DATE = 2005 #the first year that opinions were available.
-	CURRENT_DATE = Time.now.year
-	#CURRENT_DATE = 2009
-	CURRENT_YEAR = "currentYear"
 	BASEURL = "https://ecf.ksd.uscourts.gov/cgi-bin/"
-	OPINIONS = "Opinions.pl?"
+	OPINIONS = "Opinions.pl"
 	include Expect
 	def accept_host
 		"ecf.ksd.uscourts.gov"
 	end
-	# How the opinions are laid out on the website:
-	# The script at https://ecf.ksd.uscourts.gov/cgi-bin/Opinions.pl? can be passed various years, such as:
-	#   https://ecf.ksd.uscourts.gov/cgi-bin/Opinions.pl?2008
-	# The script also can be passed currentYear, which of course resolves to the current year.
-	#   https://ecf.ksd.uscourts.gov/cgi-bin/Opinions.pl?currentYear
-	# However, it is not necessary to use currentYear. One can just use the actual numerical year.
-	
-	# This method tests for all the forms of requests to the script described above.
 	def accept?(download)
-		isValid = false
-		(START_DATE .. CURRENT_DATE).each do |year|
-			isValid |= download.request_uri == BASEURL + OPINIONS + year.to_s
-		end
-		isValid |= download.request_uri == BASEURL + OPINIONS + CURRENT_YEAR
+		isValid = download.request_uri == BASEURL + OPINIONS
 	end
 	def request
-		pages = []
-		(START_DATE ... CURRENT_DATE).each do |year|
-			pages += [DownloadRequest.new(BASEURL + OPINIONS + year.to_s)]
-		end
-		pages += [DownloadRequest.new(BASEURL + OPINIONS + CURRENT_YEAR)]
+		pages = [DownloadRequest.new(BASEURL + OPINIONS)]
 	end
 	#The headers in the table of cases.
 	HEADINGS = [" Date Filed", " Case", " Opinion", " Judge"]
@@ -57,12 +39,10 @@ class Ksd
                         doc = Document.new
                         matchedDate = match(row.at("td[1]").inner_text, %r@(\d{2})/(\d{2})/(\d{4})@)
                         doc.date = Date.new(matchedDate[3].to_i, matchedDate[1].to_i, matchedDate[2].to_i)
-                        row.at("td[2]").inner_html =~ %r@(.*)<br />(.*)@
-                        doc.dockets << $1
-                        doc.name = $2
-			matchedInfo = match(row.at("td[2]").inner_html, %r@(.*)<br />(.*)@)
-			doc.dockets << matchedInfo[1]
-			doc.name = matchedInfo[2]
+			row.at("td[2]").inner_html =~ %r@(.*) Case No\. (\d+-\d+)<br />(.*)@
+			doc.subject = $1
+			doc.dockets << $2
+			doc.name = $3
                         doc.add_link("application/pdf", BASEURL + row.at("td[3] a").attributes['href'])
                         doc.court = "http://id.altlaw.org/courts/us/fed/dist/ksd"
                         receiver << doc
